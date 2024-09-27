603388752127d6b2317e96a39e0ead5f723619a8
cristian.rivera.torres@correounivalle.edu.co <cristian.rivera.torres@correounivlle.edu.co>
2024-09-27 12:21:49 -0500

    ApartmentSection, Images folder, import in App

--- InstaHome/src/App.jsx
+++ InstaHome/src/App.jsx
@@ -1,11 +1,15 @@
 import Navbar from './components/Navbar/Navbar'
+import ApartmentSection from './components/Apartments/ApartmentSection'
+
 import './App.css'
 
 function App() {
   return (
+    /*Se implementa el import de las distintas secciones de la pagina y se llaman en el return para que sean mostradas */
     <>
       <div className='overflow-x-hidden'>
       <Navbar />
+      <ApartmentSection /> 
       </div>
     </>
   )
