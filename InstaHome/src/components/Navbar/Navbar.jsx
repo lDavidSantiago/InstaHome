6751cb5edf4ec17eabb76599e7da9e09163b292e
lDavidSantiago <david.triana@correounivalle.edu.co>
2024-12-01 19:44:42 -0500

    /kick Ganzolitario98

--- InstaHome/src/components/Navbar/Navbar.jsx
+++ InstaHome/src/components/Navbar/Navbar.jsx
@@ -35,83 +35,82 @@ const Navbar = ({ setActiveSection, onLoginClick, onLogout, isLoggedIn }) => {
                 </div>
             </div>
 
-        <nav className="bg-white shadow-lg py-2">
-            <div className="container mx-auto flex justify-between items-center px-80">
-                <ul className="flex space-x-8 justify-center">
-                    <li>
-                        <a
-                            href="#"
-                            onClick={() => handleNavClick('home')}
-                            className="inline-block py-2 px-3 text-gray-600 font-semibold transition duration-300 hover:text-primary hover:border-b-2 hover:border-primary"
-                        >
-                            Home
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="#"
-                            onClick={() => handleNavClick('howItWorks')}
-                            className="inline-block py-2 px-3 text-gray-600 font-semibold transition duration-300 hover:text-primary hover:border-b-2 hover:border-primary"
-                        >
-                            How It Works
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="#"
-                            onClick={() => handleNavClick('help')}
-                            className="inline-block py-2 px-3 text-gray-600 font-semibold transition duration-300 hover:text-primary hover:border-b-2 hover:border-primary"
-                        >
-                            Help
-                        </a>
-                    </li>
-                </ul>
-                {isLoggedIn ? (
-                    <div className="relative">
-                        <button
-                            onClick={handleClick}
-                            className="bg-white rounded-full shadow-lg p-5 hover:bg-gray-200 transition-colors"
-                        >
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
-                            </svg>
-                        </button>
-                        {open && (
-                            <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-2xl py-2 z-50">
-                                <a
-                                    href="#"
-                                    className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
-                                    onClick={() => handleNavClick('profile')}
-                                >
-                                    Profile
-                                </a>
-                                <a
-                                    href="#"
-                                    className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
-                                    onClick={() => handleNavClick('RegisterHome')}
-                                >
-                                    RegisterHome
-                                </a>
-                                <a
-                                    href="#"
-                                    className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
-                                    onClick={onLogout}
-                                >
-                                    Logout
-                                </a>
-                            </div>
-                        )}
-                    </div>
-                ) : (
-                    <button
-                        onClick={onLoginClick}
-                        className="bg-primary text-white font-semibold py-2 px-4 rounded hover:bg-secondary transition duration-300"
+        <nav className="container mx-auto flex items-center justify-between px-8">
+            {/* Contenedor del menú de navegación centrado */}
+            <ul className="flex space-x-8 justify-center mx-auto">
+                {['home', 'howItWorks', 'help'].map((section) => (
+                <li key={section}>
+                    <a
+                    href="#"
+                    onClick={() => handleNavClick(section)}
+                    className="inline-block py-2 px-3 text-gray-600 font-semibold transition duration-300 hover:text-primary hover:border-b-2 hover:border-primary"
                     >
-                        Login
+                    {section.charAt(0).toUpperCase() + section.slice(1).replace(/([A-Z])/g, ' $1')}
+                    </a>
+                </li>
+                ))}
+            </ul>
+
+            {/* Botón Login o Menú desplegable alineado a la derecha */}
+            <div className="flex items-center">
+                {isLoggedIn ? (
+                <div className="relative">
+                    <button
+                    onClick={handleClick}
+                    className="bg-white rounded-full shadow-lg p-2 hover:bg-gray-200 transition-colors"
+                    >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                    >
+                        <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                        />
+                    </svg>
                     </button>
+                    {open && (
+                    <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-2xl py-2 z-50">
+                        <a
+                        href="#"
+                        className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
+                        onClick={() => handleNavClick('profile')}
+                        >
+                        Profile
+                        </a>
+                        <a
+                        href="#"
+                        className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
+                        onClick={() => handleNavClick('RegisterHome')}
+                        >
+                        RegisterHome
+                        </a>
+                        <a
+                        href="#"
+                        className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
+                        onClick={onLogout}
+                        >
+                        Logout
+                        </a>
+                    </div>
+                    )}
+                </div>
+                ) : (
+                <button
+                    onClick={onLoginClick}
+                    className="bg-primary text-white font-semibold py-2 px-4 rounded hover:bg-secondary transition duration-300"
+                >
+                    Login
+                </button>
                 )}
             </div>
-        </nav>
+            </nav>
+
     </header>
     );
 };
