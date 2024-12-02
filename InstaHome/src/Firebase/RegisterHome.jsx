31daf7ddc7593ed3e8822b83a7b30d13cb650de4
jh0j4n <jhojan.castano@correounivalle.edu.co>
2024-12-02 14:01:07 -0500

    responsive RegisterHome

--- InstaHome/src/Firebase/RegisterHome.jsx
+++ InstaHome/src/Firebase/RegisterHome.jsx
@@ -57,91 +57,125 @@ const RegisterHome = ({ isVisible, onClose, onHomeAdded }) => {
   };
 
   return (
-    <div className="w-full h-screen flex items-start">
-      <div className="relative w-1/2 h-full flex flex-col">
-        <img src={images} alt="background" className="w-full h-full object-cover" />
-          <div className="absolute inset-0 bg-black opacity-50">
-          </div>
-          <div className="absolute top-[20%] left-[10%] flex flex-col">
-            <h1 className="text-4xl text-white font-bold my-4">Success begins with a determined step!</h1>
-            <p className="text-xl text-white font-normal">Make your apartment reach the ideal people!</p>
-        </div>      
+    <div className="w-full min-h-[90vh] flex flex-col sm:flex-row">
+      {/* Sección izquierda */}
+      <div className="relative w-full sm:w-1/2 flex-shrink-0 flex flex-col">
+        <img
+          src={images}
+          alt="background"
+          className="w-full h-64 sm:h-auto object-cover"
+        />
+        <div className="absolute inset-0 bg-black opacity-50"></div>
+        <div className="absolute top-[20%] left-[10%] flex flex-col">
+          <h1 className="text-2xl sm:text-4xl text-white font-bold my-4">
+            Success begins with a determined step!
+          </h1>
+          <p className="text-base sm:text-xl text-white font-normal">
+            Make your apartment reach the ideal people!
+          </p>
+        </div>
       </div>
-      <div className= "w-1/2 h-full bg-slate-50 flex flex-col p-20 !p-0justify-between">
-        <h1 className= "text-2xl font-semibold mb-10">Registrar Un Hogar</h1>
+  
+      {/* Sección derecha */}
+      <div className="w-full sm:w-1/2 bg-slate-50 flex flex-col p-6 sm:p-20">
+        <h1 className="text-xl sm:text-2xl font-semibold mb-6 sm:mb-10">
+          Registrar Un Hogar
+        </h1>
         {message && (
-          <p className={`mb-2 text-center ${message.includes("éxito") ? "text-green-600" : "text-red-600"}`}>
+          <p
+            className={`mb-2 text-center ${
+              message.includes("éxito") ? "text-green-600" : "text-red-600"
+            }`}
+          >
             {message}
           </p>
         )}
-      <form onSubmit={handleSubmit} >
-      <div className="w-full flex flex-col mt-2">
-        <label htmlFor="descripcion" className="text-lg font-medium text-gray-700 mb-2">
-          Descripción
-        </label>
-        <textarea
-          id="descripcion"
-          name="descripcion"
-          value={formData.descripcion}
-          onChange={handleChange}
-          className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
-          rows="3"
-          required
-        ></textarea>
-      </div>
-
-      <div className="w-full flex flex-col mt-4">
-        <label htmlFor="direccion" className="text-lg font-medium text-gray-700 mb-2">
-          Dirección
-        </label>
-        <input
-          type="text"
-          id="direccion"
-          name="direccion"
-          value={formData.direccion}
-          onChange={handleChange}
-          className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
-          required
-        />
-      </div>
-      <div className="w-full flex flex-col mt-4">
-        <label htmlFor="precio" className="text-lg font-medium text-gray-700 mb-2">
-          Precio
-        </label>
-        <input
-          type="number"
-          id="precio"
-          name="precio"
-          value={formData.precio}
-          onChange={handleChange}
-          className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
-          required
-        />
-      </div>
-      <div className="w-full flex flex-col mt-4">
-        <label htmlFor="img" className="text-lg font-medium text-gray-700 mb-2">
-          Imagen
-        </label>
-        <input
-          type="url"
-          id="img"
-          name="img"
-          value={formData.img}
-          onChange={handleChange}
-          className="mb-20 block w-full p-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
-          required
-        />
-      </div>
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col justify-start" /* Cambiado de `justify-between` */
+        >
+          <div>
+            <div className="w-full flex flex-col mt-2">
+              <label
+                htmlFor="descripcion"
+                className="text-base sm:text-lg font-medium text-gray-700 mb-2"
+              >
+                Descripción
+              </label>
+              <textarea
+                id="descripcion"
+                name="descripcion"
+                value={formData.descripcion}
+                onChange={handleChange}
+                className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+                rows="3"
+                required
+              ></textarea>
+            </div>
+  
+            <div className="w-full flex flex-col mt-4">
+              <label
+                htmlFor="direccion"
+                className="text-base sm:text-lg font-medium text-gray-700 mb-2"
+              >
+                Dirección
+              </label>
+              <input
+                type="text"
+                id="direccion"
+                name="direccion"
+                value={formData.direccion}
+                onChange={handleChange}
+                className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+                required
+              />
+            </div>
+            <div className="w-full flex flex-col mt-4">
+              <label
+                htmlFor="precio"
+                className="text-base sm:text-lg font-medium text-gray-700 mb-2"
+              >
+                Precio
+              </label>
+              <input
+                type="number"
+                id="precio"
+                name="precio"
+                value={formData.precio}
+                onChange={handleChange}
+                className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+                required
+              />
+            </div>
+            <div className="w-full flex flex-col mt-4">
+              <label
+                htmlFor="img"
+                className="text-base sm:text-lg font-medium text-gray-700 mb-2"
+              >
+                Imagen
+              </label>
+              <input
+                type="url"
+                id="img"
+                name="img"
+                value={formData.img}
+                onChange={handleChange}
+                className="block w-full p-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+                required
+              />
+            </div>
+          </div>
+          {/* Mueve el botón inmediatamente después de los campos */}
           <button
             type="submit"
-            className="w-full bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700"
+            className="mt-10 w-full bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700"
             disabled={loading}
           >
             {loading ? "Registrando..." : "Registrar"}
           </button>
-      </form>
-      </div>   
-  </div>
+        </form>
+      </div>
+    </div>
   );
 };
 
